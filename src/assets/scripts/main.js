86e7d0eae9cd2fd5d9a60630b7a2fd45576eb1fc
vic13pg <vperisg@gmail.com>
2019-12-10 21:32:58 +0100

    initial commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+import 'bootstrap';
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
++( function() {
+  console.log('CV Victòria Peris Garcia -- Diseñadora Multimedia');
+} )();
